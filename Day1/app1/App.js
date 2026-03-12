d7d758d6e45a01665c7c4173e709c80a1f7f4b5d
vimal004 <2004.vimal@gmail.com>
2026-03-11 17:21:43 -0700

    .

--- Day1/app1/App.js
+++ Day1/app1/App.js
@@ -4,18 +4,10 @@ import { StyleSheet, Text, View, Image, SafeAreaView, ScrollView } from 'react-n
 export default function App() {
   console.log("Executed")
   return (
-      <ScrollView>
-      <Text>Item 1</Text>
-      <Text>Item 2</Text>
-      <Text>Item 3</Text>
-      <Text>Item 4</Text>
-      <Text>Item 5</Text>
-      <Text>Item 6</Text>
-      <Text>Item 7</Text>
-      <Text>Item 8</Text>
-      <Text>Item 9</Text>
-      <Text>Item 10</Text>
-    </ScrollView>
+    <View style={{ flex: 1 }}>
+      {/* <View style={{ flex: 1, backgroundColor: "red" }} /> */}
+      <View style={{ flex: 1, backgroundColor: "blue" }} />
+    </View>
   );
 }
 
