51524266b0307f7f7d3a36f593367c314edc5069
vimal004 <2004.vimal@gmail.com>
2026-03-11 17:24:46 -0700

    .

--- Day1/app1/App.js
+++ Day1/app1/App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView, ScrollView } from 'react-native';
+import { View } from "react-native";
 
 export default function App() {
-  console.log("Executed")
   return (
-    <View style={{ flex: 1 }}>
-      {/* <View style={{ flex: 1, backgroundColor: "red" }} /> */}
-      <View style={{ flex: 1, backgroundColor: "blue" }} />
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View style={{ width: 50, height: 50, backgroundColor: "red" }} />
+      <View style={{ width: 50, height: 50, backgroundColor: "blue" }} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
